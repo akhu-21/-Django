ab664b1f6c73f39500bd5d7a60b8364b06a347da
M  P Akhil kumar <madsoulakhil@gmail.com>
2026-03-30 22:17:43 +0530

    added html for cart & checkout page

--- ecomerce/store/views.py
+++ ecomerce/store/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+
 def store(request):
-    return render(request, 'store/store.html')
+    context = {}
+    return render(request, 'store/store.html',context)
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    context = {}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    return render(request,'store/checkout.html')
+    context = {}
+    return render(request,'store/checkout.html', context)
